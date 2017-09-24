fdab14f15fc10084f5973879c343c884a8bf2100
ndeveat <ndeveat@gmail.com>
2017-09-24 22:02:59 +0900

    add push activity

---
+++ app/src/main/java/com/ndeveat/pinpost/Activity/PushActivity.kt
@@ -0,0 +1,14 @@
+package com.ndeveat.pinpost.Activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.ndeveat.pinpost.R
+
+class PushActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_push)
+    }
+}
