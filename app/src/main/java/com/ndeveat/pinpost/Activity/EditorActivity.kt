12ff5c3bbb88319f168b85b9ee53b63682886104
ndeveat <ndeveat@gmail.com>
2017-09-25 12:11:05 +0900

    add Ted library

--- app/src/main/java/com/ndeveat/pinpost/Activity/EditorActivity.kt
+++ app/src/main/java/com/ndeveat/pinpost/Activity/EditorActivity.kt
@@ -1,6 +1,8 @@
 package com.ndeveat.pinpost.Activity
 
+import android.Manifest
 import android.graphics.PorterDuff
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -19,6 +21,12 @@ import kotlinx.android.synthetic.main.editor_container.view.*
 import kotlinx.android.synthetic.main.main_container.view.*
 import kotlinx.android.synthetic.main.toolbar.view.*
 import org.jetbrains.anko.intentFor
+import gun0912.tedbottompicker.TedBottomPicker
+import java.util.ArrayList
+import android.widget.Toast
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
+
 
 class EditorActivity : AppCompatActivity() {
 
@@ -26,6 +34,8 @@ class EditorActivity : AppCompatActivity() {
     var mEditorEmptyView: View? = null
     var mEditorContents: TextView? = null
     var mEditorContainer: ScrollView? = null
+
+    var bottomSheetDialogFragment: TedBottomPicker? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,11 +59,41 @@ class EditorActivity : AppCompatActivity() {
         mEditorEmptyView?.setOnClickListener {
             mEditorContents!!.requestFocus()
         }
+        // 권한 체크하기
+        val permissionlistener = object : PermissionListener {
+            override fun onPermissionGranted() {
+                // Toast.makeText(this@EditorActivity, "Permission Granted", Toast.LENGTH_SHORT).show()
+
+                bottomSheetDialogFragment = TedBottomPicker.Builder(this@EditorActivity)
+                        .setOnMultiImageSelectedListener(object : TedBottomPicker.OnMultiImageSelectedListener {
+                            override fun onImagesSelected(uriList: ArrayList<Uri>?) {
+
+                            }
+                        })
+                        .setPeekHeight(800)
+                        .showTitle(false)
+                        .setCompleteButtonText("Done")
+                        .setEmptySelectionText("No Select")
+                        .create()
+            }
+
+            override fun onPermissionDenied(deniedPermissions: ArrayList<String>) {
+                Toast.makeText(this@EditorActivity, "Permission Denied\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        TedPermission.with(this)
+                .setPermissionListener(permissionlistener)
+                .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
+                .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE)
+                .check()
 
         // Set push button
         editor_push_button.setOnClickListener {
             Log.d("push", "push")
             val intent = intentFor<PushActivity>()
+            intent.putExtra("Title", mEditorTitle!!.text)
+            intent.putExtra("Contents", mEditorContents!!.text)
             startActivity(intent)
         }
     }
