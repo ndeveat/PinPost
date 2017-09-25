88f9e91a2cdd2311a9d6f9886cc20b3a280db4d4
ndeveat <ndeveat@gmail.com>
2017-09-25 11:40:06 +0900

    add editor activity setting

--- app/src/main/java/com/ndeveat/pinpost/Activity/EditorActivity.kt
+++ app/src/main/java/com/ndeveat/pinpost/Activity/EditorActivity.kt
@@ -3,23 +3,35 @@ package com.ndeveat.pinpost.Activity
 import android.graphics.PorterDuff
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.Toolbar
 import android.util.Log
 import android.view.MenuItem
+import android.view.View
+import android.widget.ScrollView
+import android.widget.TextView
 
 import com.ndeveat.pinpost.R
 import kotlinx.android.synthetic.main.activity_editor.*
+import kotlinx.android.synthetic.main.editor_container.*
+import kotlinx.android.synthetic.main.editor_container.view.*
 import kotlinx.android.synthetic.main.main_container.view.*
 import kotlinx.android.synthetic.main.toolbar.view.*
 import org.jetbrains.anko.intentFor
 
 class EditorActivity : AppCompatActivity() {
 
+    var mEditorTitle: TextView? = null
+    var mEditorEmptyView: View? = null
+    var mEditorContents: TextView? = null
+    var mEditorContainer: ScrollView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_editor)
 
+        // Set home button
         val supportToolbar = toolbar
         setSupportActionBar(supportToolbar.pinpost_toolbar)
 
@@ -27,6 +39,18 @@ class EditorActivity : AppCompatActivity() {
         supportActionBar?.setHomeButtonEnabled(true);
         supportActionBar?.setDisplayHomeAsUpEnabled(true);
 
+        mEditorContainer = editor_scrollview
+        mEditorContents = editor_contents_text
+        mEditorContents?.isFocusableInTouchMode = true
+
+        // Set Editor variable
+        mEditorTitle = editor_title
+        mEditorEmptyView = editor_empty_view
+        mEditorEmptyView?.setOnClickListener {
+            mEditorContents!!.requestFocus()
+        }
+
+        // Set push button
         editor_push_button.setOnClickListener {
             Log.d("push", "push")
             val intent = intentFor<PushActivity>()
---
+++ app/src/main/java/com/ndeveat/pinpost/Base/EditText.kt
@@ -0,0 +1,29 @@
+package com.ndeveat.pinpost.Base
+
+import android.content.Context
+import android.graphics.Typeface
+import android.util.AttributeSet
+import android.widget.EditText
+import android.widget.TextView
+
+/**
+ * Created by ndeveat on 2017. 9. 25..
+ */
+
+open class EditText : EditText {
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyle: Int) : super(context, attrs, defStyle) {
+        init()
+    }
+
+    fun init() {
+        this.typeface = Typeface.createFromAsset(context.assets, "fonts/NanumBarunGothicUltraLight.otf")
+    }
+}
